198a59302d93a006a591f190cc77b60d65506fde
sathya reddy <sathyareddy@sathyas-MacBook-Pro.local>
2024-11-21 13:05:18 +0530

    removed strivers

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-import striversA2ZDSA.BasicMath;
 
 public class Main {
     public static void main(String[] args){
-        BasicMath bm = new BasicMath();
-        bm.isPalindrome(12321);
+
     }
 }
